f01c6ea34939638d41e1dad6595a294858fdb4db
PRIYANSHU74340 <priyanshu74340@gmail.com>
2025-09-08 15:10:53 +0530

    conffile

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -3,7 +3,7 @@
     appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
     appwriteDbId : String(import.meta.env.VITE_APPWRITE_DB_ID),
     appwriteCollectionId : String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
-    appwriteApiKey : String(import.meta.env.VITE_APPWRITE_API_KEY),
+    
 }
 
 export default conf;
